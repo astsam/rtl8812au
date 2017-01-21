647020663dfb64f49f7a51d9765fa9959c642749
Sergei Makarenkov <serg@no-problem.cc>
2017-01-21 23:44:28 +0300

    Use in_compat_syscall() instead of is_compat_task() if available

--- os_dep/linux/ioctl_linux.c
+++ os_dep/linux/ioctl_linux.c
@@ -13120,8 +13120,12 @@ static int rtw_ioctl_standard_wext_private(struct net_device *dev, struct ifreq 
 static int rtw_ioctl_wext_private(struct net_device *dev, struct ifreq *rq)
 {
 #ifdef CONFIG_COMPAT
-	if (is_compat_task())
-		return rtw_ioctl_compat_wext_private(dev, rq);
+#ifdef in_compat_syscall
+	if(in_compat_syscall())
+#else
+	if(is_compat_task())
+#endif
+		return rtw_ioctl_compat_wext_private( dev, rq );
 	else
 #endif /* CONFIG_COMPAT */
 		return rtw_ioctl_standard_wext_private(dev, rq);
