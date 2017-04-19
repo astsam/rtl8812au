088f9b7f84394a32b067596ebd9216d0420e6bf9
astsam <serg@no-problem.cc>
2017-04-19 19:20:27 +0300

    Stop exporting unnecessary symbols

--- os_dep/linux/rtw_radiotap.c
+++ os_dep/linux/rtw_radiotap.c
@@ -161,7 +161,6 @@ int rtw_ieee80211_radiotap_iterator_init(
 
 	return 0;
 }
-EXPORT_SYMBOL(rtw_ieee80211_radiotap_iterator_init);
 
 static void find_ns(struct ieee80211_radiotap_iterator *iterator,
 		    uint32_t oui, uint8_t subns)
@@ -367,4 +366,4 @@ int rtw_ieee80211_radiotap_iterator_next(
 			return 0;
 	}
 }
-EXPORT_SYMBOL(rtw_ieee80211_radiotap_iterator_next);
+
