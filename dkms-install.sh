e6b82ab1777916a159dd5e6a5768e4ebe8e647d7
paspro <panos.asproulis@renuda.com>
2017-05-18 15:36:40 +0200

    DKMS Support

---
+++ dkms-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must run this with superuser priviliges.  Try \"sudo ./dkms-install.sh\"" 2>&1
+  exit 1
+else
+  echo "About to run dkms install steps..."
+fi
+
+DRV_DIR=Realtek-8812au-8814au
+DRV_NAME=rtl8812au
+DRV_VERSION=5.1.5
+
+cp -r ../${DRV_DIR} /usr/src/${DRV_NAME}-${DRV_VERSION}
+
+dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
+dkms build -m ${DRV_NAME} -v ${DRV_VERSION}
+dkms install -m ${DRV_NAME} -v ${DRV_VERSION}
+RESULT=$?
+
+echo "Finished running dkms install steps."
+
+exit $RESULT
+
