69d81d85c2482750708514358db1087691670ea6
zebulon2 <zeb@zebulon.org.uk>
2017-07-15 13:26:04 +0100

    Pull request from MilhouseVH
    https://github.com/zebulon2/rtl8812au/pull/2

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -3654,7 +3654,7 @@ static int rtw_cfg80211_add_monitor_if(_adapter *padapter, char *name, struct ne
 	strncpy(mon_ndev->name, name, IFNAMSIZ);
 	mon_ndev->name[IFNAMSIZ - 1] = 0;
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 9))
-	mon_ndev->needs_free_netdev = true;
+	mon_ndev->needs_free_netdev = false;
 	mon_ndev->priv_destructor = rtw_ndev_destructor;
 #else
  	mon_ndev->destructor = rtw_ndev_destructor;
