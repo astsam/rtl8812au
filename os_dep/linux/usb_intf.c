f8108dbd4e339a64e009da6eb5de5ac2367755b1
Christian kimocoder <christianbremvaag@gmail.com>
2017-08-24 17:02:47 +0200

    More USB VID/PID
    * Added support for Alfa - AWUS036AC
    * Added support for Amped Wireless ACA1
    * Added support for TP-Link - Archer T4U AC1300
    * Added support for TP-Link - Arhcer T4UH AC1300
    * Added support for TP-Link - T4UHP
    * Added support for TP-Link - T4UHP (other)

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -167,9 +167,15 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	{USB_DEVICE(0x13b1, 0x003f), .driver_info = RTL8812}, /* Linksys - WUSB6300 */
 	{USB_DEVICE(0x2357, 0x0101), .driver_info = RTL8812}, /* TP-Link - Archer T4U */
 	{USB_DEVICE(0x2357, 0x0103), .driver_info = RTL8812}, /* TP-Link - T4UH */
+	{USB_DEVICE(0x2357, 0x010D), .driver_info = RTL8812}, /* TP-Link - Archer T4U AC1300 */
+	{USB_DEVICE(0x2357, 0x010E), .driver_info = RTL8812}, /* TP-Link - Archer T4UH AC1300 */
+	{USB_DEVICE(0x2357, 0x010F), .driver_info = RTL8812}, /* TP-Link - T4UHP */
+	{USB_DEVICE(0x2357, 0x0122), .driver_info = RTL8812}, /* TP-Link - T4UHP (other) */
 	{USB_DEVICE(0x20f4, 0x805b), .driver_info = RTL8812}, /* TRENDnet - */
 	{USB_DEVICE(0x0411, 0x025d), .driver_info = RTL8812}, /* Buffalo - WI-U3-866D */
 	{USB_DEVICE(0x050D, 0x1109), .driver_info = RTL8812}, /* Belkin F9L1109 - SerComm */
+	{USB_DEVICE(0x148F, 0x9097), .driver_info = RTL8812}, /* Amped Wireless ACA1 */
+	{USB_DEVICE(0x0BDA, 0x8812), .driver_info = RTL8812}, /* Alfa - AWUS036AC */
 #endif
 
 #ifdef CONFIG_RTL8821A
