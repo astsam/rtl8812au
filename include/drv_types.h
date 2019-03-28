bdf81c17847095817db69fc5c1034f4057214f7e
kimocoder <christian@aircrack-ng.org>
2019-03-29 00:07:51 +0100

    Fix includes to support linux

--- include/drv_types.h
+++ include/drv_types.h
@@ -102,7 +102,7 @@ typedef struct _ADAPTER _adapter, ADAPTER, *PADAPTER;
 #include <rtw_ioctl_query.h>
 #include <osdep_intf.h>
 #include <rtw_eeprom.h>
-#include <sta_info.h>
+#include "sta_info.h"
 #include <rtw_event.h>
 #include <rtw_mlme_ext.h>
 #include <rtw_mi.h>
