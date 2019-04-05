04c4350de1e43ebeab7b935434ddec1fd5a3f83f
kimocoder <christian@aircrack-ng.org>
2019-04-06 00:47:44 +0200

    Disable IPv6 to work + sysctl restart

--- dkms-install.sh
+++ dkms-install.sh
@@ -20,4 +20,15 @@ RESULT=$?
 
 echo "Finished running dkms install steps."
 
+
+	if echo "net.ipv6.conf.all.disable_ipv6 = 1
+  net.ipv6.conf.default.disable_ipv6 = 1
+  net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf; then
+		echo "Disabled IPv6 Successfuly "
+		sysctl -p
+	else
+		echo "Could not disable IPv6"
+	fi
+
+
 exit $RESULT
