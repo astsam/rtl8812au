astsam/rtl8812au

commit 3fcba157f907430c19ec01b156446e799b0c73aa

dkms-install: Don't hardcode parent folder

dkms-install.sh
@@ -7,11 +7,11 @@ else
   echo "About to run dkms install steps..."
 fi
 
-DRV_DIR=rtl8812au
+DRV_DIR='pwd'
 DRV_NAME=rtl8812au
 DRV_VERSION=5.3.4
 
-cp -r $(pwd) /usr/src/${DRV_NAME}-${DRV_VERSION}
+cp -r ${DRV_DIR} /usr/src/${DRV_NAME}-${DRV_VERSION}
 
 dkms add -m ${DRV_NAME} -v ${DRV_VERSION}
 dkms build -m ${DRV_NAME} -v ${DRV_VERSION}

commit 6e61837391b8cdcef0ada607c9af5bd50a790372

Actually run `pwd` into a variable, instead of just quoting 'pwd'
This fixes a bug in DKMS compilation introduced in commit 3fcba157.

dkms-install.sh
@@ -7,7 +7,7 @@ else
   echo "About to run dkms install steps..."
 fi
 
-DRV_DIR='pwd'
+DRV_DIR="$(pwd)"
 DRV_NAME=rtl8812au
 DRV_VERSION=5.3.4
 