3de2dde344c6e0785661e230c0dc92fa67275650
shyan003 <45695077+shyan003@users.noreply.github.com>
2019-05-24 15:50:23 +0800

    Update usb_intf.c

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -219,6 +219,7 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 	{USB_DEVICE(0x0BDA, 0xA811), .driver_info = RTL8821}, /* GMYLE - AC450 */
 	{USB_DEVICE(0x3823, 0x6249), .driver_info = RTL8821}, /* Obihai - OBiWiFi */
 	{USB_DEVICE(0x2357, 0x0122), .driver_info = RTL8821}, /* TP Link - T2U Nano */
+	{USB_DEVICE(0x2357, 0x011E), .driver_info = RTL8821}, /* TP Link - T2U Nano */
 #endif
 
 #ifdef CONFIG_RTL8192E
