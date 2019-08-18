b3aef6d2900b21ffc8d2a0645129f464e336f785
kimocoder <christian@aircrack-ng.org>
2019-08-18 04:45:55 +0200

    Fixed a issue when DEBUG was enabled

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -1439,7 +1439,7 @@ static u8 is_rtw_ndev(struct net_device *ndev)
 static int rtw_ndev_notifier_call(struct notifier_block *nb, unsigned long state, void *ptr)
 {
 	struct net_device *ndev;
-	_adapter *adapter = rtw_netdev_priv(ndev);
+	_adapter *adapter
 
 	if (ptr == NULL)
 		return NOTIFY_DONE;
@@ -1452,6 +1452,8 @@ static int rtw_ndev_notifier_call(struct notifier_block *nb, unsigned long state
 
 	if (ndev == NULL)
 		return NOTIFY_DONE;
+
+	adapter = rtw_netdev_priv(ndev);
 
 	if (!is_rtw_ndev(ndev))
 		return NOTIFY_DONE;
