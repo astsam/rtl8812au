e9daed618769e183569fa4f49dd71d3b1bcc2f39
kimocoder <christian@aircrack-ng.org>
2019-08-18 04:43:53 +0200

    Fix interface rename issue

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -1439,6 +1439,7 @@ static u8 is_rtw_ndev(struct net_device *ndev)
 static int rtw_ndev_notifier_call(struct notifier_block *nb, unsigned long state, void *ptr)
 {
 	struct net_device *ndev;
+	_adapter *adapter = rtw_netdev_priv(ndev);
 
 	if (ptr == NULL)
 		return NOTIFY_DONE;
@@ -1460,6 +1461,8 @@ static int rtw_ndev_notifier_call(struct notifier_block *nb, unsigned long state
 	switch (state) {
 	case NETDEV_CHANGENAME:
 		//rtw_adapter_proc_replace(ndev);
+		strncpy(adapter->old_ifname, ndev->name, IFNAMSIZ);
+		adapter->old_ifname[IFNAMSIZ-1] = 0;
 		break;
 	#ifdef CONFIG_NEW_NETDEV_HDL
 	case NETDEV_PRE_UP :
