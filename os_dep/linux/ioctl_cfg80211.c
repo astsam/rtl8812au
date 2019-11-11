7ad5618762a9cc1b3fe42336550b324b6e5dfd39
kimocoder <christian@aircrack-ng.org>
2019-11-11 04:07:55 +0100

    Fix build support to older GCCs by adding fallthroughs

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -2407,6 +2407,7 @@ static int cfg80211_rtw_change_iface(struct wiphy *wiphy,
 	case NL80211_IFTYPE_P2P_CLIENT:
 		is_p2p = _TRUE;
 	#endif
+	// Intentional fallthrough
 	case NL80211_IFTYPE_STATION:
 		networkType = Ndis802_11Infrastructure;
 
@@ -2431,6 +2432,7 @@ static int cfg80211_rtw_change_iface(struct wiphy *wiphy,
 	case NL80211_IFTYPE_P2P_GO:
 		is_p2p = _TRUE;
 	#endif
+	// Intentional fallthrough
 	case NL80211_IFTYPE_AP:
 		networkType = Ndis802_11APMode;
 
