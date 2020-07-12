cddf06aa3929eb4072ded52912578fa4083a7c0b
Christian Bremvåg <christian@aircrack-ng.org>
2020-07-12 04:23:08 +0200

    Fix build when CONFIG_USB_SUSPEND is defined

--- include/hal_data.h
+++ include/hal_data.h
@@ -1057,4 +1057,31 @@ int rtw_halmac_deinit_adapter(struct dvobj_priv *);
 
 #define RX_SMOOTH_FACTOR	Rx_Smooth_Factor
 
+
+
+extern unsigned char	RTW_WPA_OUI[];
+extern unsigned char	WMM_OUI[];
+extern unsigned char	WPS_OUI[];
+extern unsigned char	P2P_OUI[];
+extern unsigned char	WFD_OUI[];
+
+int pm_netdev_open(struct net_device *pnetdev,u8 bnormal);
+void *scdb_findEntry(_adapter *priv, unsigned char *macAddr, unsigned char *ipAddr);
+void dhcp_flag_bcast(_adapter *priv, struct sk_buff *skb);
+int nat25_handle_frame(_adapter *priv, struct sk_buff *skb);
+int nat25_db_handle(_adapter *priv, struct sk_buff *skb, int method);
+void nat25_db_expire(_adapter *priv);
+extern unsigned char REALTEK_96B_IE[];
+int rtw_change_ifname(_adapter *padapter, const char *ifname);
+void indicate_wx_scan_complete_event(_adapter *padapter);
+u8 rtw_do_join(_adapter *padapter);
+int _netdev_open(struct net_device *pnetdev);
+int netdev_open (struct net_device *pnetdev);
+u8 key_2char2num(u8 hch, u8 lch);
+u8 key_2char2num(u8 hch, u8 lch);
+void macstr2num(u8 *dst, u8 *src);
+#ifdef CONFIG_AUTOSUSPEND
+void autosuspend_enter(_adapter* padapter);
+#endif
+
 #endif /* __HAL_DATA_H__ */
